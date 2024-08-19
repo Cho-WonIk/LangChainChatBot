8dbd939a4b8f5cbea7218c2a1cea280c4dd4035a
cwi0212 <cwi0212@naver.com>
2024-08-19 21:21:19 +0900

    ui수정1차

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,7 @@
 import streamlit as st
 
-# 첫 페이지 설정
+# 첫 번째 페이지 구성
 def page_one():
-    st.sidebar.title("메뉴")
-    menu_items = ["홈", "보드", "분석", "설정", "로그아웃"]
-    for item in menu_items:
-        st.sidebar.button(item)
-    
     with st.container():
         st.text_area("설명을 입력하세요", height=300)
         st.text_input("간단한 메모")
@@ -16,10 +11,11 @@ def page_one():
 
     st.button("저장")
 
-# 두 번째 페이지 설정
+# 두 번째 페이지 구성
 def page_two():
-    st.sidebar.checkbox("이주 간격")
-    st.sidebar.text("다음 메모")
+    with st.container():
+        st.checkbox("이주 간격")
+        st.text("다음 메모")
     
     with st.container():
         st.text_area("주요 내용", height=300)
@@ -27,11 +23,14 @@ def page_two():
     with st.container():
         st.text_area("AI 의견", height=100)
 
-# 앱 실행
-if __name__ == "__main__":
+# 스트림릿 사이드바 메뉴 설정
+def main():
     page = st.sidebar.selectbox("페이지 선택", ["첫 페이지", "두 번째 페이지"])
 
     if page == "첫 페이지":
         page_one()
     elif page == "두 번째 페이지":
         page_two()
+
+if __name__ == "__main__":
+    main()
